9e4ffe70656e33f30b6041938e3b7d5872c942b1
Ashkan-Zanjani <ash333d@gmail.com>
2021-08-10 02:23:24 +0400

    Testing

--- src/main/java/com/vaadin/app/views/about/AboutView.java
+++ src/main/java/com/vaadin/app/views/about/AboutView.java
@@ -1,7 +1,12 @@
 package com.vaadin.app.views.about;
 
+import com.vaadin.app.api.ApiClient;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H5;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.app.views.MainLayout;
@@ -10,9 +15,18 @@ import com.vaadin.app.views.MainLayout;
 @Route(value = "about", layout = MainLayout.class)
 public class AboutView extends Div {
 
+    private Div div = new Div();
+
     public AboutView() {
-        addClassName("about-view");
-        add(new Text("Content placeholder"));
+
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.getStyle().set("border", "1px solid #EEEEEE").set("padding","100px");
+        layout.add(new H5("com-1"), new H5("com-2"), new H5("com-3"));
+        layout.setWidth("100%");
+        div.add(new Text("Text"));
+        layout.add(div);
+        layout.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.END);
+        add(layout);
     }
 
 }
