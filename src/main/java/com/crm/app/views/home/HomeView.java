8d99820a490e4fe2749be7529bb03ffad0447f31
Ashkan-Zanjani <ash333d@gmail.com>
2021-08-13 20:00:55 +0400

    fixed UI

--- src/main/java/com/crm/app/views/home/HomeView.java
+++ src/main/java/com/crm/app/views/home/HomeView.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayoutVariant;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -22,6 +23,9 @@ public class HomeView extends HorizontalLayout {
 
     private HorizontalLayout header;
     private SplitLayout mainLayout;
+    private Button addColumnButton;
+    private FormLayout form;
+
 
     public HomeView() {
         addClassName("home-view");
@@ -30,6 +34,7 @@ public class HomeView extends HorizontalLayout {
         header.setMargin(true);
         header.add(new H4("SQLite schema tables"));
         mainLayout = new SplitLayout();
+        mainLayout.addThemeVariants(SplitLayoutVariant.LUMO_SMALL);
         mainLayout.setSplitterPosition(25);
         showTables();
         createTableForm();
@@ -49,15 +54,23 @@ public class HomeView extends HorizontalLayout {
 
     private void createTableForm() {
         VerticalLayout formLayout = new VerticalLayout(new Label("Create new table"));
-        FormLayout form = new FormLayout();
-
+        form = new FormLayout();
         TextField tableName = new TextField();
+        TextField column = new TextField();
+
         tableName.setLabel("Table name");
+        column.setPlaceholder("name");
         tableName.setPlaceholder("Table 1");
-        Button button = new Button("Add");
-        form.add(tableName,button);
-        formLayout.add(form);
-        formLayout.setWidth("50%");
+        addColumnButton = new Button("Add");
+        addColumnButton.setMaxWidth("30px");
+        addColumnButton.getStyle().set("margin-left","-5px");
+
+        form.add(column,addColumnButton);
+        form.setMaxWidth("410px");
+        form.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("1px", 1),
+                new FormLayout.ResponsiveStep("220px", 2));
+        formLayout.add(tableName,form);
         mainLayout.addToSecondary(formLayout);
     }
 }
